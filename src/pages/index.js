5b187ff1daba84d31956a78ad1067e2926d270f4
nic-lan <narghy82@yahoo.it>
2019-08-11 02:09:46 +0100

    add first links

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,33 @@
 import React from "react"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLinkedin, faSoundcloud, faGithub } from '@fortawesome/free-brands-svg-icons'
+
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
+const personalLinks = [
+  { icon: faLinkedin, url: "https://www.linkedin.com/in/nicolaslanguille/"},
+  { icon: faSoundcloud, url: "https://soundcloud.com/raum149"},
+  { icon: faGithub, url: "https://github.com/nic-lan"},
+].map((e) => {
+  return(
+    <a href={e['url']}>
+      <FontAwesomeIcon
+        icon={e['icon']}
+        size='2x'
+      />
+    </a>
+  )
+})
+
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <h1>niclan</h1>
+    {personalLinks}
   </Layout>
 )
 
